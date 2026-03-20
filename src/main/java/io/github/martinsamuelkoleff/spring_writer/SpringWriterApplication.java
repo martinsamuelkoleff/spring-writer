3bad499dfe37bd6f1d0dfc4bc864bb1ad9704563
martinkoleff <koleffmartinsamuel@gmail.com>
2026-03-20 15:46:15 -0300

    Add Spring Initializr project files

---
+++ src/main/java/io/github/martinsamuelkoleff/spring_writer/SpringWriterApplication.java
@@ -0,0 +1,13 @@
+package io.github.martinsamuelkoleff.spring_writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWriterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/martinsamuelkoleff/spring_writer/SpringWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.martinsamuelkoleff.spring_writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
