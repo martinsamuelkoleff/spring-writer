39cf6bcc980c9d427165a4aef86f25967e2cba18
martinkoleff <koleffmartinsamuel@gmail.com>
2026-03-28 15:28:04 -0300

    fix: enable  public comments in /blog/*/comments

--- src/main/java/io/github/martinsamuelkoleff/spring_writer/config/SecurityConfig.java
+++ src/main/java/io/github/martinsamuelkoleff/spring_writer/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,13 +17,18 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(Customizer.withDefaults())
+	        .csrf(csrf -> csrf
+	                .ignoringRequestMatchers("/blog/*/comments")
+	            )
             .httpBasic(Customizer.withDefaults())
             .formLogin(Customizer.withDefaults())
             .logout(logout -> logout
                     .logoutUrl("/logout")
                     .logoutSuccessUrl("/")
                     .permitAll()
+                )
+            .sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 )
             .authorizeHttpRequests((authorize) -> authorize
             		.requestMatchers(
